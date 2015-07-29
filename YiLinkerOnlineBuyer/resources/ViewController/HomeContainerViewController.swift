41a69e8e26aa90c44098354c12732864a83ff0ff
Alvin John <alvinjohntandoc@gmail.com>
2015-07-29 11:07:49 +0800

    Implement changing of content view in the main controller view.

--- YiLinkerOnlineBuyer/resources/ViewController/HomeContainerViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/HomeContainerViewController.swift
@@ -8,17 +8,91 @@
 
 import UIKit
 
-class HomeContainerViewController: UIViewController {
+class HomeContainerViewController: UIViewController, UITabBarDelegate {
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var tabBar: UITabBar!
+    
+    
+    var homePageCollectionViewController: HomePageCollectionViewController?
+    var searchViewContoller: SearchViewController?
+    var circularMenuViewController: CircularMenuViewController?
+    var wishlisViewController: WishlistViewController?
+    var cartViewController: CartViewController?
+    
+    var viewControllers = [UIViewController]()
+    
+    var selectedChildViewController: UIViewController?
+    var contentViewFrame: CGRect?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tabBar.delegate = self
+        initViewControllers()
+        
+        setSelectedViewControllerWithIndex(0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        contentViewFrame = contentView.bounds
+    }
+    
+    // This function is for executing child view logic code
+    func setSelectedViewControllerWithIndex(index: Int) {
+        let viewController: UIViewController = viewControllers[index]
+        setSelectedViewController(viewController)
+    }
+    
+    func setSelectedViewController(viewController: UIViewController) {
+        if !(selectedChildViewController?.isEqual(viewController) != nil) {
+            if self.isViewLoaded() {
+                selectedChildViewController?.willMoveToParentViewController(self)
+                selectedChildViewController?.view.removeFromSuperview()
+                selectedChildViewController?.removeFromParentViewController()            }
+        }
+        self.view.layoutIfNeeded()
+        self.addChildViewController(viewController)
+        viewController.view.frame = contentViewFrame!
+        contentView.addSubview(viewController.view)
+        viewController.didMoveToParentViewController(self)
+        selectedChildViewController = viewController
+    }
+    
+    func initViewControllers() {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        homePageCollectionViewController = storyBoard.instantiateViewControllerWithIdentifier("HomePageCollectionViewController") as? HomePageCollectionViewController
+        searchViewContoller = storyBoard.instantiateViewControllerWithIdentifier("SearchViewController") as? SearchViewController
+        circularMenuViewController = storyBoard.instantiateViewControllerWithIdentifier("CircularMenuViewController") as? CircularMenuViewController
+        wishlisViewController = storyBoard.instantiateViewControllerWithIdentifier("WishlistViewController") as? WishlistViewController
+        cartViewController = storyBoard.instantiateViewControllerWithIdentifier("CartViewController") as? CartViewController
+        
+        viewControllers.append(homePageCollectionViewController!)
+        viewControllers.append(searchViewContoller!)
+        viewControllers.append(circularMenuViewController!)
+        viewControllers.append(wishlisViewController!)
+        viewControllers.append(cartViewController!)
+    }
+    
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem!) {
+        if item.tag != 2{
+            setSelectedViewControllerWithIndex(item.tag)
+        } else {
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let animatedViewController: CircularMenuViewController = storyBoard.instantiateViewControllerWithIdentifier("CircularMenuViewController") as! CircularMenuViewController
+            
+            animatedViewController.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+            animatedViewController.providesPresentationContextTransitionStyle = true
+            animatedViewController.definesPresentationContext = true
+            animatedViewController.view.backgroundColor = UIColor.clearColor()
+            
+            self.presentViewController(animatedViewController, animated: false, completion: nil)
+        }
+    }
+    
 }
-
