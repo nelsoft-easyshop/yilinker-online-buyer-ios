c500425910935e8d729d529a59c264bf7398d85f
Alvin John <alvinjohntandoc@gmail.com>
2015-08-03 18:51:25 +0800

    Implemented dummy data for home page collection view.

--- YiLinkerOnlineBuyer/resources/View/ScrollableCollectionViewCell.swift
+++ YiLinkerOnlineBuyer/resources/View/ScrollableCollectionViewCell.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ScrollableCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var productModels: [HomePageProductModel]!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let cellNib: UINib = UINib(nibName: "ProductWithCenterNameCollectionViewCell", bundle: nil)
+        self.collectionView.registerNib(cellNib, forCellWithReuseIdentifier: "ProductWithCenterNameCollectionViewCell")
+        self.collectionView.backgroundColor = UIColor.clearColor()
+        self.collectionView.reloadData()
     }
-
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.productModels.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let scrollableProductCollectionViewCell: ProductWithCenterNameCollectionViewCell = self.collectionView?.dequeueReusableCellWithReuseIdentifier("ProductWithCenterNameCollectionViewCell", forIndexPath: indexPath) as! ProductWithCenterNameCollectionViewCell
+        
+        
+        return scrollableProductCollectionViewCell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        println("click!!!")
+    }
 }
