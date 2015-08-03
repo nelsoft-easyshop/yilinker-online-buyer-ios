f142d2d843b4682b3522750c514eca7b94d796f4
Alvin John <alvinjohntandoc@gmail.com>
2015-08-03 18:36:13 +0800

    Added homepage product model.

--- YiLinkerOnlineBuyer/resources/Model/HomePageProductModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/HomePageProductModel.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
-class HomePageProductModel: NSObject {
-   
+class HomePageProductModel: ProductModel {
+    var productAction: String = ""
+    var productTargetUrl: String = ""
+    
+     init(productName: String, productImageURL: NSURL, productOriginalPrice: NSNumber, productDiscountedPrice: NSNumber, productDiscountPercentage: NSNumber, productSlug: String, productAction: String, productTargetUrl: String) {
+        
+        super.init(productName: "", productImageURL: NSURL(string: "")!, productOriginalPrice: 0, productDiscountedPrice: 0, productDiscountPercentage: 0, productSlug: "")
+        
+        self.productName = productName
+        self.productImageURL = productImageURL
+        self.productOriginalPrice = productOriginalPrice
+        self.productDiscountedPrice = productDiscountedPrice
+        self.productDiscountPercentage = productDiscountPercentage
+        self.productSlug = productSlug
+        self.productAction = productAction
+        self.productTargetUrl = productTargetUrl
+    }
 }
