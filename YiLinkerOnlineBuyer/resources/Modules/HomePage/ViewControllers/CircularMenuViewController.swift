63dfcd831273371ba5f812f65f7139e21a45385f
Alvin John <alvinjohntandoc@gmail.com>
2015-08-04 16:37:40 +0800

    Implemented circular menu.

--- YiLinkerOnlineBuyer/resources/Modules/HomePage/ViewControllers/CircularMenuViewController.swift
+++ YiLinkerOnlineBuyer/resources/Modules/HomePage/ViewControllers/CircularMenuViewController.swift
@@ -9,22 +9,15 @@
 import UIKit
 
 class CircularMenuViewController: UIViewController {
-
-    @IBOutlet weak var shadowView: UIView!
     
-    @IBOutlet var helpButton: RoundedButton!
-    @IBOutlet var followedSellerButton: RoundedButton!
-    @IBOutlet weak var messagingButton: RoundedButton!
-    @IBOutlet weak var customizeShoppingButton: RoundedButton!
-    @IBOutlet weak var todaysPromoButton: RoundedButton!
-    @IBOutlet weak var categoryButton: RoundedButton!
-    @IBOutlet weak var roundedProfileImageView: RoundedImageView!
     @IBOutlet weak var roundedButton: RoundedButton!
     
     @IBOutlet weak var dimView: UIView!
     
+    var buttonImages: [String] = [""]
     
-    @IBOutlet weak var logoutSemiRoundedButton: SemiRoundedButton!
+    
+  /* @IBOutlet weak var logoutSemiRoundedButton: SemiRoundedButton!
     @IBOutlet weak var categoryLabel: RoundedLabel!
     @IBOutlet weak var todaysPromoLabel: RoundedLabel!
     @IBOutlet weak var customizeShoppingLabel: RoundedLabel!
@@ -32,16 +25,16 @@ class CircularMenuViewController: UIViewController {
     @IBOutlet weak var followedSellerLabel: RoundedLabel!
     @IBOutlet weak var helpLabel: RoundedLabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var messageTitleButton: UIButton!
+    @IBOutlet weak var messageTitleButton: UIButton! */
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.initDimView()
         self.hideControlTitlesNotAnimated()
         
-        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tapProfile")
+        /*let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tapProfile")
         self.roundedProfileImageView.userInteractionEnabled = true
-        self.roundedProfileImageView.addGestureRecognizer(tapGesture)
+        self.roundedProfileImageView.addGestureRecognizer(tapGesture)*/
     }
     
     override func viewDidLayoutSubviews() {
@@ -62,51 +55,51 @@ class CircularMenuViewController: UIViewController {
         let delay = 0.01 * Double(NSEC_PER_SEC)
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
         dispatch_after(time, dispatch_get_main_queue()) {
-            UIView.animateWithDuration(0.5, delay: 0.0, options: nil, animations: {
-                self.roundedButton.transform = CGAffineTransformMakeScale(1.2, 1.2)
+            UIView.animateWithDuration(0.2, delay: 0.0, options: nil, animations: {
+                //self.roundedButton.transform = CGAffineTransformMakeScale(1.2, 1.2)
                 }, completion: nil)
         }
         
-        let helpFrame: CGRect = helpButton.frame
+       /* let helpFrame: CGRect = helpButton.frame
         let followedSellerFrame: CGRect = followedSellerButton.frame
         let messagingFrame: CGRect = messagingButton.frame
         let customizeShoppingFrame: CGRect = customizeShoppingButton.frame
         let todayPromoFrame: CGRect = todaysPromoButton.frame
         let categoryButtonFrame: CGRect = categoryButton.frame
-        let roundedProfileImageViewFrame: CGRect = roundedProfileImageView.frame
+        let roundedProfileImageViewFrame: CGRect = roundedProfileImageView.frame */
         
         self.hideControlNotAnimated()
         
         UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 3.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: ({
-            self.helpButton.frame = helpFrame
+           /* self.helpButton.frame = helpFrame
             self.followedSellerButton.frame = followedSellerFrame
             self.messagingButton.frame = messagingFrame
             self.customizeShoppingButton.frame = customizeShoppingFrame
             self.todaysPromoButton.frame = todayPromoFrame
             self.categoryButton.frame = categoryButtonFrame
             self.roundedButton.frame = roundedProfileImageViewFrame
-            self.roundedProfileImageView.frame = roundedProfileImageViewFrame
+            self.roundedProfileImageView.frame = roundedProfileImageViewFrame*/
             self.dimView.alpha = 0.6
-            self.showTitles()
+            //self.showTitles()
         }), completion: nil)
     }
     
     
     private func hideControlNotAnimated() {
-        self.helpButton.frame = CGRectMake(helpButton.frame.origin.x, view.frame.size.height + 50, helpButton.frame.size.width, helpButton.frame.size.height)
+       /* self.helpButton.frame = CGRectMake(helpButton.frame.origin.x, view.frame.size.height + 50, helpButton.frame.size.width, helpButton.frame.size.height)
         self.followedSellerButton.frame = CGRectMake(self.followedSellerButton.frame.origin.x, self.view.frame.size.height + 50, self.followedSellerButton.frame.size.width, self.followedSellerButton.frame.size.height)
         self.messagingButton.frame = CGRectMake(self.messagingButton.frame.origin.x, self.view.frame.size.height + 50, self.messagingButton.frame.size.width, self.messagingButton.frame.size.height)
         self.customizeShoppingButton.frame = CGRectMake(self.customizeShoppingButton.frame.origin.x, self.view.frame.size.height + 50, self.customizeShoppingButton.frame.size.width, self.customizeShoppingButton.frame.size.height)
         self.todaysPromoButton.frame = CGRectMake(self.customizeShoppingButton.frame.origin.x, view.frame.size.height + 50, self.customizeShoppingButton.frame.size.width, self.customizeShoppingButton.frame.size.height)
         self.categoryButton.frame = CGRectMake(self.categoryButton.frame.origin.x, self.view.frame.size.height + 50, self.categoryButton.frame.size.width, self.categoryButton.frame.size.height)
-        self.roundedProfileImageView.frame = CGRectMake(self.roundedProfileImageView.frame.origin.x, self.view.frame.size.height + 50, self.roundedProfileImageView.frame.size.width, self.roundedProfileImageView.frame.size.height)
+        self.roundedProfileImageView.frame = CGRectMake(self.roundedProfileImageView.frame.origin.x, self.view.frame.size.height + 50, self.roundedProfileImageView.frame.size.width, self.roundedProfileImageView.frame.size.height) */
     }
     
     
      func dissmissViewControllerAnimated() {
         UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 3.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: ({
             self.hideControlNotAnimated()
-            self.roundedProfileImageView.transform = CGAffineTransformMakeScale(0.1, 0.1)
+          /*  self.roundedProfileImageView.transform = CGAffineTransformMakeScale(0.1, 0.1)
             
             self.helpButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
             self.followedSellerButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
@@ -114,13 +107,13 @@ class CircularMenuViewController: UIViewController {
             self.customizeShoppingButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
             self.todaysPromoButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
             self.categoryButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
-            self.roundedProfileImageView.transform = CGAffineTransformMakeScale(0.1, 0.1)
+            self.roundedProfileImageView.transform = CGAffineTransformMakeScale(0.1, 0.1) */
             self.dimView.alpha = 0.0
             
             self.hideControlTitlesNotAnimated()
             
             UIView.animateWithDuration(0.5, delay: 0.0, options: nil, animations: {
-                self.roundedButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
+                //self.roundedButton.transform = CGAffineTransformMakeScale(0.1, 0.1)
                 }, completion: {(value: Bool) in
                     self.dismissViewControllerAnimated(false, completion: nil)
             })
@@ -128,13 +121,13 @@ class CircularMenuViewController: UIViewController {
         }), completion: {
             (value: Bool) in
             
-            self.helpButton.hidden = true
+          /*  self.helpButton.hidden = true
             self.followedSellerButton.hidden = true
             self.messagingButton.hidden = true
             self.customizeShoppingButton.hidden = true
             self.todaysPromoButton.hidden = true
             self.categoryButton.hidden = true
-            self.roundedProfileImageView.hidden = true
+            self.roundedProfileImageView.hidden = true */
             
             
         })
@@ -153,7 +146,7 @@ class CircularMenuViewController: UIViewController {
     }
     
     func showTitles() {
-        self.logoutSemiRoundedButton.alpha = 1.0
+      /*  self.logoutSemiRoundedButton.alpha = 1.0
         self.categoryLabel.alpha = 1.0
         self.todaysPromoLabel.alpha = 1.0
         self.customizeShoppingLabel.alpha = 1.0
@@ -161,11 +154,11 @@ class CircularMenuViewController: UIViewController {
         self.followedSellerLabel.alpha = 1.0
         self.helpLabel.alpha = 1.0
         self.nameLabel.alpha = 1.0
-        self.messageTitleButton.alpha = 1.0
+        self.messageTitleButton.alpha = 1.0*/
     }
     
     func hideControlTitlesNotAnimated() {
-        self.logoutSemiRoundedButton.alpha = 0.0
+       /* self.logoutSemiRoundedButton.alpha = 0.0
         self.categoryLabel.alpha = 0.0
         self.todaysPromoLabel.alpha = 0.0
         self.customizeShoppingLabel.alpha = 0.0
@@ -173,7 +166,7 @@ class CircularMenuViewController: UIViewController {
         self.followedSellerLabel.alpha = 0.0
         self.helpLabel.alpha = 0.0
         self.nameLabel.alpha = 0.0
-        self.messageTitleButton.alpha = 0.0
+        self.messageTitleButton.alpha = 0.0*/
     }
     
     
@@ -202,7 +195,7 @@ class CircularMenuViewController: UIViewController {
     }
     
     @IBAction func categoriesAction(sender: AnyObject) {
-        println("categories")
+
     }
     
 }
