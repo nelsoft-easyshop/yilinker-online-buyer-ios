c872d67492e9ad2183852b8cfaf9bf37295e060b
Alvin John <alvinjohntandoc@gmail.com>
2015-08-07 23:02:16 +0800

    Added model for seller.

---
+++ YiLinkerOnlineBuyer/resources/Helper/Constansts.swift
@@ -0,0 +1,9 @@
+//
+//  Constansts.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 8/7/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import Foundation
---
+++ YiLinkerOnlineBuyer/resources/Modules/HomePage/View/GenericCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GenericCollectionViewCell.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 8/6/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class GenericCollectionViewCell: UICollectionViewCell {
+    
+}
