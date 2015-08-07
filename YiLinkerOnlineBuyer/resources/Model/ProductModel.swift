00aef26d976707fb8a0236af5b2e4a83cba050dd
Alvin John <alvinjohntandoc@gmail.com>
2015-08-07 23:02:43 +0800

    Added model for product.

--- YiLinkerOnlineBuyer/resources/Model/ProductModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/ProductModel.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ProductModel: NSObject {
-    var productName: String = ""
-    var productImageURL: NSURL?
-    var productOriginalPrice: String = ""
-    var productDiscountedPrice: String = ""
-    var productDiscountPercentage: String = ""
-    var productTarget: String = ""
+    var name: String = ""
+    var imageURL: NSURL?
+    var originalPrice: String = ""
+    var discountedPrice: String = ""
+    var discountPercentage: String = ""
+    var target: String = ""
     
-    init(productName: String, productImageURL: NSURL, productOriginalPrice: String, productDiscountedPrice: String, productDiscountPercentage: String, productTarget: String) {
-        self.productName = productName
-        self.productImageURL = productImageURL
-        self.productOriginalPrice = productOriginalPrice
-        self.productDiscountedPrice = productDiscountedPrice
-        self.productDiscountPercentage = productDiscountPercentage
-        self.productTarget = productTarget
+    init(name: String, imageURL: NSURL, originalPrice: String, discountedPrice: String, discountPercentage: String, target: String) {
+        self.name = name
+        self.imageURL = imageURL
+        self.originalPrice = originalPrice
+        self.discountedPrice = discountedPrice
+        self.discountPercentage = discountPercentage
+        self.target = target
     }
 }
