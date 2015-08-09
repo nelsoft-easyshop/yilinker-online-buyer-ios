c13f707fa454994ea9d7b619547e842fe1e45608
Alvin John <alvinjohntandoc@gmail.com>
2015-08-09 13:41:26 +0800

    Implemented facebook login.

--- YiLinkerOnlineBuyer/resources/ViewController/LoginViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/LoginViewController.swift
@@ -7,9 +7,65 @@
 //
 
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
    
+    @IBOutlet weak var facebookButton: FBSDKLoginButton!
     override func viewDidLoad() {
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            self.returnUserData()
+        } else {
+            self.facebookButton.readPermissions = ["public_profile", "email", "user_friends"]
+            self.facebookButton.delegate = self
+        }
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
         
+        if ((error) != nil) {
+            // Process error
+        } else if result.isCancelled {
+            // Handle cancellations
+        } else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email") {
+                // Do work
+                self.returnUserData()
+            }
+        }
+    
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    
+    func returnUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            if ((error) != nil) {
+                // Process error
+                println("Error: \(error)")
+            } else {
+                if let val: AnyObject = result.valueForKey("name") {
+                    let userName : NSString = result.valueForKey("name") as! NSString
+                    println("fetched user: \(userName)")
+                }
+                
+                if let val: AnyObject = result.valueForKey("id") {
+                    let uid : NSString = result.valueForKey("id") as! NSString
+                    println("fetched userID: \(uid)")
+                }
+               
+                if let val: AnyObject = result.valueForKey("email") {
+                    let email : NSString = result.valueForKey("email") as! NSString
+                    println("fetched userID: \(email)")
+                } else {
+                    println("Email is not available!")
+                }
+            }
+        })
     }
 }
