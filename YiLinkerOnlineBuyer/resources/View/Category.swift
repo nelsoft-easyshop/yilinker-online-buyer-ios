278b97de56821656bdc6f737469d31029da1456f
Alvin John <alvinjohntandoc@gmail.com>
2015-08-10 17:56:42 +0800

    Added enum file.

---
+++ YiLinkerOnlineBuyer/resources/View/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 8/9/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import Foundation
