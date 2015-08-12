aa9b886824716e6842be36c9922a8249e64b88e9
Edward <edward.estrada@easyshop.ph>
2015-08-12 13:29:57 +0800

    Implemented Search Behavior

---
+++ YiLinkerOnlineBuyer/resources/View/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by @EasyShop.ph on 8/11/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
