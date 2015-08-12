7d642866df11fa514136aa04a604e365e13e7158
Alvin John <alvinjohntandoc@gmail.com>
2015-08-12 15:48:40 +0800

    Added reachiblity helper class for checking internet connection.

---
+++ YiLinkerOnlineBuyer/resources/Model/RegisterModel.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 8/12/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import Foundation
