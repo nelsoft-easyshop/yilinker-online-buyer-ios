cfcf377a0e5b8bd7f130818a86dcb86e4515aee1
Alvin John <alvinjohntandoc@gmail.com>
2015-08-12 18:39:21 +0800

    Enhance checking of internet connection.

---
+++ YiLinkerOnlineBuyer/resources/View/EmptyView.swift
@@ -0,0 +1,13 @@
+//
+//  EmptyView.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 8/12/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import Cocoa
+
+class EmptyView: UIView {
+
+}
