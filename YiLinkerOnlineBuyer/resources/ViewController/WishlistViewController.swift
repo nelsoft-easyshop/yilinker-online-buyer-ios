2a7bba7f56be80259a7513a801d08a3731b827e6
jpChan15 <john.chan@easyshop.ph>
2015-08-12 20:56:20 +0800

    Implemented wishlist. (issue #13)

--- YiLinkerOnlineBuyer/resources/ViewController/WishlistViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/WishlistViewController.swift
@@ -8,30 +8,87 @@
 
 import UIKit
 
-class WishlistViewController: UIViewController {
-
+class WishlistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, WishlistTableViewCellDelegate {
+    
+    @IBOutlet var wishlistTableView: UITableView!
+    
+    @IBOutlet var wishListCounterLabel: UILabel!
+    
     let viewControllerIndex = 2
+    
+    var tableData: [String] = ["Ferrari", "BMW", "Mitsubishi", "Lambo"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        wishlistTableView.delegate = self;
+        wishlistTableView.dataSource = self;
+        
+        var nib = UINib(nibName: "WishlistTableViewCell", bundle: nil)
+        wishlistTableView.registerNib(nib, forCellReuseIdentifier: "WishlistTableViewCell")
+        
+        populateWishListTableView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func populateWishListTableView (){
+        updateCounterLabel()
     }
-    */
-
+    
+    func updateCounterLabel() {
+        if tableData.count < 2 {
+            wishListCounterLabel.text = "You have \(tableData.count) item in your wishlist"
+        } else{
+            wishListCounterLabel.text = "You have \(tableData.count) items in your wishlist"
+        }
+        
+    }
+    
+    // MARK: - Table View Delegate
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell 	{
+        var cell:WishlistTableViewCell = self.wishlistTableView.dequeueReusableCellWithIdentifier("WishlistTableViewCell") as! WishlistTableViewCell
+        
+        cell.productNameLabel.text = tableData[indexPath.row]
+        cell.delegate = self
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Row \(indexPath.row) selected")
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 105
+    }
+    
+    // MARK: - Wishlist Table View Delegate
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        NSNotificationCenter.defaultCenter().postNotificationName("SwipeForOptionsCellEnclosingTableViewDidBeginScrollingNotification", object: scrollView)
+    }
+    
+    // MARK: - Wishlist Table View Delegate
+    func deleteButtonActionForIndex(sender: AnyObject){
+        var pathOfTheCell: NSIndexPath = wishlistTableView.indexPathForCell(sender as! UITableViewCell)!
+        var rowOfTheCell: Int = pathOfTheCell.row
+        tableData.removeAtIndex(pathOfTheCell.row);
+        wishlistTableView.deleteRowsAtIndexPaths([pathOfTheCell], withRowAnimation: UITableViewRowAnimation.Fade)
+        NSLog("rowofthecell %d", rowOfTheCell);
+        updateCounterLabel()
+    }
+    
+    func addToCartButtonActionForIndex(sender: AnyObject){
+        var pathOfTheCell: NSIndexPath = wishlistTableView.indexPathForCell(sender as! UITableViewCell)!
+        var rowOfTheCell: Int = pathOfTheCell.row
+        NSLog("rowofthecell %d", rowOfTheCell);
+    }
+    
 }
