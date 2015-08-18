0663300bf6ece6061a0bdc5035077c607271c0df
jpChan15 <john.chan@easyshop.ph>
2015-08-18 17:53:58 +0800

    Implemented search UI.

--- YiLinkerOnlineBuyer/resources/ViewController/SearchViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/SearchViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     let viewControllerIndex = 1
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchResultTableView: UITableView!
+    
+    var tableData: [SearchSuggestionModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.initializeViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +28,91 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initializeViews() {
+        // Connect all delegate prototypes
+        self.searchBar.delegate = self
+        self.searchResultTableView.dataSource = self
+        self.searchResultTableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+        let titleDict = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+            as [NSObject : AnyObject]
+        self.navigationController!.navigationBar.titleTextAttributes = titleDict
+        
+        // Remove trailing cells
+        self.searchResultTableView.tableFooterView = UIView(frame: CGRectZero)
+        
+        // Remove NavigationBar shadow
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+        
+        //Register Nib to Tableview
+        var nib = UINib(nibName: "SearchSuggestionTableViewCell", bundle: nil)
+        searchResultTableView.registerNib(nib, forCellReuseIdentifier: "SearchSuggestionTableViewCell")
+        
+        if(self.tableData.isEmpty) {
+            var temp: SearchSuggestionModel = SearchSuggestionModel(suggestion: "Browse by Category", imageURL: "SearchBrowseCategory") as SearchSuggestionModel
+            
+            tableData.append(temp)
+            self.searchResultTableView.reloadData()
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    // Mark: - UISearchBarDelegate
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        // Show Scope bar with cancel
+        self.searchBar.showsScopeBar = true
+        self.searchBar.sizeToFit()
+        self.searchBar.setShowsCancelButton(true, animated: true)
+        
+        return true
+    }
+    
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        self.searchBar.showsScopeBar = false
+        self.searchBar.sizeToFit()
+        self.searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchResultTableView.reloadData()
+    }
+    
+    // Mark: - UITableViewDataSource methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = searchResultTableView.dequeueReusableCellWithIdentifier("SearchSuggestionTableViewCell") as! SearchSuggestionTableViewCell
+        
+        var tempModel: SearchSuggestionModel = tableData[indexPath.row]
+        
+        cell.suggestionTextLabel?.text = tempModel.suggestion
+        if tempModel.imageURL == "SearchBrowseCategory" {
+            cell.suggestionImageView.image = UIImage(named: "SearchBrowseCategory")
+        } else {
+            cell.suggestionImageView.sd_setImageWithURL(NSURL(string: tempModel.imageURL), placeholderImage: UIImage(named: "dummy-placeholder"))
+        }
+        
+        return cell
+    }
+    
+    // Mark: - UITableViewDelegate methods
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var tempModel: SearchSuggestionModel = tableData[indexPath.row]
+        println(tempModel.suggestion)
+        println(tempModel.imageURL)
+
+    }
 
 }
