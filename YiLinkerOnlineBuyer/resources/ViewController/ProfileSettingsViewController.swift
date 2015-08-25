e4054cf90ca538e5c3a5beec2831aa8fcf83aa9e
jpChan15 <john.chan@easyshop.ph>
2015-08-25 12:14:10 +0800

    Added title and back button in my points and settings

--- YiLinkerOnlineBuyer/resources/ViewController/ProfileSettingsViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/ProfileSettingsViewController.swift
@@ -21,6 +21,8 @@ class ProfileSettingsViewController: UIViewController, UITableViewDataSource, UI
         super.viewDidLoad()
 
         initializeViews()
+        titleView()
+        backButton()
         registerNibs()
     }
 
@@ -36,6 +38,26 @@ class ProfileSettingsViewController: UIViewController, UITableViewDataSource, UI
         }
         
         tableView.tableFooterView = UIView(frame: CGRectZero)
+    }
+    
+    func titleView() {
+        self.title = "Settings"
+    }
+    
+    func backButton() {
+        var backButton:UIButton = UIButton.buttonWithType(UIButtonType.Custom) as! UIButton
+        backButton.frame = CGRectMake(0, 0, 40, 40)
+        backButton.addTarget(self, action: "back", forControlEvents: UIControlEvents.TouchUpInside)
+        backButton.setImage(UIImage(named: "back-white"), forState: UIControlState.Normal)
+        var customBackButton:UIBarButtonItem = UIBarButtonItem(customView: backButton)
+        
+        let navigationSpacer: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FixedSpace, target: nil, action: nil)
+        navigationSpacer.width = -20
+        self.navigationItem.leftBarButtonItems = [navigationSpacer, customBackButton]
+    }
+    
+    func back() {
+        self.navigationController!.popViewControllerAnimated(true)
     }
     
     func registerNibs() {
