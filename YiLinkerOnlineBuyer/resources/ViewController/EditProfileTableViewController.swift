2d9f0155dd1611c35f74d5efaef0e19a292dd36b
jpChan15 <john.chan@easyshop.ph>
2015-08-25 11:57:43 +0800

    Fixed layout of profile dasboard.

--- YiLinkerOnlineBuyer/resources/ViewController/EditProfileTableViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/EditProfileTableViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class EditProfileTableViewController: UITableViewController, EditProfileAddPhotoTableViewCellDelegate, EditProfileAddressTableViewCellDelegate {
+class EditProfileTableViewController: UITableViewController, UINavigationControllerDelegate, EditProfileAddPhotoTableViewCellDelegate, EditProfileAddressTableViewCellDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate  {
     
     let addPhotoCell: String = "EditProfileAddPhotoTableViewCell"
     let personalInfoCell: String = "EditProfilePersonalInformationTableViewCell"
     let addressCell: String = "EditProfileAddressTableViewCell"
     let accountCell: String = "EditProfileAccountInformationTableViewCell"
-        
+    
+    var photoIndexPath: NSIndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         initializeViews()
         registerNibs()
     }
@@ -53,25 +55,28 @@ class EditProfileTableViewController: UITableViewController, EditProfileAddPhoto
         self.tableView.registerNib(nibAccount, forCellReuseIdentifier: accountCell)
     }
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
         return 4
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCellWithIdentifier(addPhotoCell, forIndexPath: indexPath) as! EditProfileAddPhotoTableViewCell
             cell.delegate = self
+            
+            photoIndexPath = indexPath
+            
             return cell
         } else if indexPath.row == 1 {
             let cell = tableView.dequeueReusableCellWithIdentifier(personalInfoCell, forIndexPath: indexPath) as! EditProfilePersonalInformationTableViewCell
@@ -98,55 +103,136 @@ class EditProfileTableViewController: UITableViewController, EditProfileAddPhoto
             return 270
         }
     }
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    // Return NO if you do not want the specified item to be editable.
+    return true
     }
     */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    // Return NO if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
     }
     */
     
     // MARK: - EditProfileAddPhotoTableViewCellDelegate
     func addPhotoAction(sender: AnyObject) {
         println("addPhotoAction")
+        /* Supports UIAlert Controller */
+        if( controllerAvailable()){
+            handleIOS8()
+        } else {
+            var actionSheet:UIActionSheet
+            if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+                actionSheet = UIActionSheet(title: "Add photo", delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: nil,otherButtonTitles:"Select photo from library", "Take a picture")
+            } else {
+                actionSheet = UIActionSheet(title: "Add photo", delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: nil,otherButtonTitles:"Select photo from library")
+            }
+            actionSheet.delegate = self
+            actionSheet.showInView(self.view)
+            /* Implement the delegate for actionSheet */
+        }
+    }
+    
+    func handleIOS8(){
+        let imageController = UIImagePickerController()
+        imageController.editing = false
+        imageController.delegate = self
+        let alert = UIAlertController(title: "Add photo", message: "", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        let libButton = UIAlertAction(title: "Select photo from library", style: UIAlertActionStyle.Default) { (alert) -> Void in
+            imageController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentViewController(imageController, animated: true, completion: nil)
+        }
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            let cameraButton = UIAlertAction(title: "Take a picture", style: UIAlertActionStyle.Default) { (alert) -> Void in
+                println("Take Photo")
+                imageController.sourceType = UIImagePickerControllerSourceType.Camera
+                self.presentViewController(imageController, animated: true, completion: nil)
+                
+            }
+            alert.addAction(cameraButton)
+        } else {
+            println("Camera not available")
+            
+        }
+        let cancelButton = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (alert) -> Void in
+            println("Cancel Pressed")
+        }
+        
+        alert.addAction(libButton)
+        alert.addAction(cancelButton)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        let cell:EditProfileAddPhotoTableViewCell = self.tableView.cellForRowAtIndexPath(photoIndexPath!) as! EditProfileAddPhotoTableViewCell
+        
+        cell.profileImageView.hidden = false
+        cell.profileImageView.image = image
+        cell.addPhotoLabel.text = "Edit Photo"
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        println("Title : \(actionSheet.buttonTitleAtIndex(buttonIndex))")
+        println("Button Index : \(buttonIndex)")
+        let imageController = UIImagePickerController()
+        imageController.editing = false
+        imageController.delegate = self;
+        if( buttonIndex == 1){
+            imageController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        } else if(buttonIndex == 2){
+            imageController.sourceType = UIImagePickerControllerSourceType.Camera
+        } else {
+            
+        }
+        self.presentViewController(imageController, animated: true, completion: nil)
+    }
+    
+    func controllerAvailable() -> Bool {
+        if let gotModernAlert: AnyClass = NSClassFromString("UIAlertController") {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     
     // MARK: - EditProfileAddressTableViewCellDelegate
@@ -154,7 +240,7 @@ class EditProfileTableViewController: UITableViewController, EditProfileAddPhoto
         println("changeAddressAction")
     }
     
-    // Hide Keyboard 
+    // Hide Keyboard
     func hideKeyboard() {
         self.view.endEditing(true)
     }
