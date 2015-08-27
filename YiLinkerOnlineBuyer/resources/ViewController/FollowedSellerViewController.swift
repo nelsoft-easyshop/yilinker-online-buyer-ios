bea0a32b51daa0662d7eeac9aef3424b1b90279e
Rj <rj.constantino@easyshop.ph>
2015-08-27 18:19:05 +0800

    Applied refresh token grant type.

--- YiLinkerOnlineBuyer/resources/ViewController/FollowedSellerViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/FollowedSellerViewController.swift
@@ -106,7 +106,7 @@ class FollowedSellerViewController: UIViewController, EmptyViewDelegate {
         let url: String = "http://online.api.easydeal.ph/api/v1/login"
         let params: NSDictionary = ["client_id": Constants.Credentials.clientID,
             "client_secret": Constants.Credentials.clientSecret,
-            "grant_type": Constants.Credentials.grantRefresh,
+            "grant_type": Constants.Credentials.grantRefreshToken,
             "refresh_token": SessionManager.refreshToken()]
         
         let manager = APIManager.sharedInstance
