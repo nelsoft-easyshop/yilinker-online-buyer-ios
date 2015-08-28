d526d87d70ea69017f2233bee4c981e7be2a2f7a
Yhel <joriel.fronda@easyshop.ph>
2015-08-28 12:34:14 -0700

    Added new guest checkout container view controller.

---
+++ YiLinkerOnlineBuyer/resources/Xib/GuestCheckoutHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GuestCheckoutHeaderTableViewCell.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Joriel Oller Fronda on 8/28/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class GuestCheckoutHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
