4fd8fd6bc2d5c63bb72142b9557482a177cb6851
jpChan15 <john.chan@easyshop.ph>
2015-08-29 20:49:37 +0800

    Resolved app crashes when retrieving wihlist data from web.

--- YiLinkerOnlineBuyer/resources/Model/WishlistProductDetailsModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/WishlistProductDetailsModel.swift
@@ -91,8 +91,10 @@ class WishlistProductDetailsModel {
                 image = tempVar
             }
             
-            if let tempVar = dictionary["images"] as? NSArray {
-                images = tempVar as! [String]
+            for tempVar in dictionary["images"] as! NSArray {
+                if let temp = tempVar["imageLocation"] as? String {
+                    images.append(temp)
+                }
             }
             
             if let tempVar = dictionary["shortDescription"] as? String {
