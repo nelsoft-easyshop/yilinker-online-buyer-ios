a741ba19c941f362473778611a0dd9d4a04d12fa
Yhel <joriel.fronda@easyshop.ph>
2015-09-09 18:57:28 +0800

    Implemented transactions api.

--- YiLinkerOnlineBuyer/resources/Model/TransactionModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/TransactionModel.swift
@@ -79,9 +79,53 @@ class TransactionModel: NSObject {
         var total_quantity: [String] = []
         var product_name: [String] = []
         var product_count: [String] = []
+        var message: String = ""
+        var isSuccessful: Bool = false
         
         if dictionary.isKindOfClass(NSDictionary) {
-            if let
+            if let tempVar = dictionary["message"] as? String {
+                message = tempVar
+            }
+            
+            if let tempVar = dictionary["isSuccessful"] as? Bool {
+                isSuccessful = tempVar
+            }
+            
+            if let value: AnyObject = dictionary["data"] {
+                let orders: NSArray = value["orders"] as! NSArray
+                for order in orders as! [NSDictionary] {
+                    
+                    let dateComponents = NSDateComponents()
+                    
+                    var dates = order["date_added"] as! String
+                    let dateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                    let date: NSDate = dateFormatter.dateFromString(dates)!
+                    
+                    let dateFormatter1 = NSDateFormatter()
+                    dateFormatter1.dateFormat = "MMMM dd, yyyy"
+                    let dateAdded = dateFormatter1.stringFromDate(date)
+                    
+                    order_id.append(order["order_id"] as! String)
+                    date_added.append(dateAdded)
+                    invoice_number.append(order["invoice_number"] as! String)
+                    payment_type.append(order["payment_type"] as! String)
+                    payment_method_id.append(order["payment_method_id"] as! String)
+                    order_status.append(order["order_status"] as! String)
+                    order_status_id.append(order["order_status_id"] as! String)
+                    total_price.append(order["total_price"] as! String)
+                    total_unit_price.append(order["total_unit_price"] as! String)
+                    total_item_price.append(order["total_item_price"] as! String)
+                    total_handling_fee.append(order["total_handling_fee"] as! String)
+                    total_quantity.append(order["total_quantity"] as! String)
+                    product_name.append(order["product_names"] as! String)
+                    product_count.append(order["product_count"] as! String)
+                }
+            }
         }
+        
+        let transactionModel = TransactionModel(order_id: order_id, date_added: date_added, invoice_number: invoice_number, payment_type: payment_type, payment_method_id: payment_method_id, order_status: order_status, order_status_id: order_status_id, total_price: total_price, total_unit_price: total_unit_price, total_item_price: total_item_price, total_handling_fee: total_handling_fee, total_quantity: total_quantity, product_name: product_name, product_count: product_count)
+        
+        return transactionModel
     }
 }
