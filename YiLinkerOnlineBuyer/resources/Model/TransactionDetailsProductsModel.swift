fd3aa331d9af3760d2759d32821f877e5770e33f
Yhel <joriel.fronda@easyshop.ph>
2015-09-11 15:05:03 +0800

    Added model for transaction product details

---
+++ YiLinkerOnlineBuyer/resources/Model/TransactionDetailsProductsModel.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionDetailsProductsModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Joriel Oller Fronda on 9/11/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class TransactionDetailsProductsModel: NSObject {
+   
+}
