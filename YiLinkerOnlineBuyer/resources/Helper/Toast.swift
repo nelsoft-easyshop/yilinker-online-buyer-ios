2b6061ee44a3f768d91def50d38ce98d2454b267
ajtandoc <alvin.tandoc@easyshop.ph>
2015-10-27 15:31:23 +0800

    Added delay helper.

---
+++ YiLinkerOnlineBuyer/resources/Helper/Toast.swift
@@ -0,0 +1,13 @@
+//
+//  Toast.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 10/27/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class Toast: NSObject {
+   
+}
