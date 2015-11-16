acd026d71a8dad8355fc45f5deea871344266ea9
Yhel <joriel.fronda@easyshop.ph>
2015-11-16 23:16:20 +0800

    Resolved bugs - added pagination for activity logs.

--- YiLinkerOnlineBuyer/resources/Model/ActivityLogItemModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/ActivityLogItemModel.swift
@@ -9,5 +9,44 @@
 import UIKit
 
 class ActivityLogItemModel: NSObject {
-   
+    
+    var date: String = ""
+    var type: String = ""
+    var text: String = ""
+    
+    init(date: String, type: String, text: String){
+        self.date = date
+        self.type = type
+        self.text = text
+    }
+    
+    class func parseDataWithDictionary(dictionary: AnyObject) -> ActivityLogItemModel {
+        
+        var date: String = ""
+        var type: String = ""
+        var text: String = ""
+        
+        if dictionary.isKindOfClass(NSDictionary) {
+            if dictionary["date"] != nil {
+                let tempDict: NSDictionary = dictionary["date"] as! NSDictionary
+                if let tempVar = tempDict["date"] as? String {
+                    date = tempVar
+                }
+            }
+            
+            if dictionary["type"] != nil {
+                if let tempVar = dictionary["type"] as? String {
+                    type = tempVar
+                }
+            }
+            
+            if dictionary["text"] != nil {
+                if let tempVar = dictionary["text"] as? String {
+                    text = tempVar
+                }
+            }
+        }
+        
+        return ActivityLogItemModel(date: date, type: type, text: text)
+    }
 }
--- YiLinkerOnlineBuyer/resources/Model/ActivityLogItemsModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/ActivityLogItemsModel.swift
@@ -9,5 +9,48 @@
 import UIKit
 
 class ActivityLogItemsModel: NSObject {
-   
+    
+    var isSuccessful: Bool = false
+    var message: String = ""
+    var activities: [ActivityLogItemModel] = []
+    
+    init(isSuccessful: Bool, message: String, activities: [ActivityLogItemModel]) {
+        self.isSuccessful = isSuccessful
+        self.message = message
+        self.activities = activities
+    }
+    
+    class func parseDataWithDictionary(dictionary: AnyObject) -> ActivityLogItemsModel {
+        
+        var isSuccessful: Bool = false
+        var message: String = ""
+        var activities: [ActivityLogItemModel] = []
+        
+        if dictionary.isKindOfClass(NSDictionary) {
+            
+            if dictionary["isSuccessful"] != nil {
+                if let tempVar = dictionary["isSuccessful"] as? Bool {
+                    isSuccessful = tempVar
+                }
+            }
+            
+            if dictionary["message"] != nil {
+                if let tempVar = dictionary["message"] as? String {
+                    message = tempVar
+                }
+            }
+            
+            if dictionary["data"] != nil {
+                if let tempDictOuter = dictionary["data"] as? NSDictionary {
+                    for subValue in tempDictOuter["activities"] as! NSArray {
+                        let model: ActivityLogItemModel = ActivityLogItemModel.parseDataWithDictionary(subValue as! NSDictionary)
+                        activities.append(model)
+                    }
+                }
+            }
+        }
+        
+        return ActivityLogItemsModel(isSuccessful: isSuccessful, message: message, activities: activities)
+    }
+    
 }
