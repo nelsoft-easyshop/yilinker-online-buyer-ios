f39f6676582276c431eef11a0169f0268d301b1d
Yhel <joriel.fronda@easyshop.ph>
2015-11-16 23:15:33 +0800

    Resolved bugs - added pagination for activity logs.

---
+++ YiLinkerOnlineBuyer/resources/Model/ActivityLogItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityLogItemModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Joriel Oller Fronda on 11/16/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class ActivityLogItemModel: NSObject {
+   
+}
---
+++ YiLinkerOnlineBuyer/resources/Model/ActivityLogItemsModel.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityLogItemsModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Joriel Oller Fronda on 11/16/15.
+//  Copyright (c) 2015 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class ActivityLogItemsModel: NSObject {
+   
+}
