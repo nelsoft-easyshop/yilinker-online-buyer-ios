6a033c6ef3886d97a2da2545b5265755f33e5e20
ajtandoc <alvin.tandoc@easyshop.ph>
2015-11-21 23:16:49 +0800

    Resolved unrequired fields.

--- YiLinkerOnlineBuyer/resources/View/GuestCheckoutTableViewCell.swift
+++ YiLinkerOnlineBuyer/resources/View/GuestCheckoutTableViewCell.swift
@@ -20,36 +20,24 @@ class GuestCheckoutTableViewCell: UITableViewCell, UITextFieldDelegate {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var mobileNumberTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var unitNumberTextField: UITextField!
-    @IBOutlet weak var buildingNumberTextField: UITextField!
-    @IBOutlet weak var streetNumberTextField: UITextField!
-    @IBOutlet weak var streetNameTextField: UITextField!
-    @IBOutlet weak var subdivisionTextField: UITextField!
+    
     @IBOutlet weak var provinceTextField: UITextField!
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var barangayTextField: UITextField!
     @IBOutlet weak var zipCodeTextField: UITextField!
-    @IBOutlet weak var additionalInfoTextField: UITextField!
-    
+    @IBOutlet weak var streetNameTextField: UITextField!
     
     @IBOutlet weak var firstNameLabel: UILabel!
     @IBOutlet weak var lastNameLabel: UILabel!
     @IBOutlet weak var mobileNumberLabel: UILabel!
     @IBOutlet weak var emailAddressLabel: UILabel!
-    
-    @IBOutlet weak var streetNumberLabel: UILabel!
     @IBOutlet weak var streetNameLabel: UILabel!
-    @IBOutlet weak var zipCodelabel: UILabel!
     
-    @IBOutlet weak var unitNoLabel: UILabel!
-    @IBOutlet weak var buildingNameLabel: UILabel!
-    @IBOutlet weak var subdivisionLabel: UILabel!
+    @IBOutlet weak var zipCodelabel: UILabel!
+
     @IBOutlet weak var provinceLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var barangayLabel: UILabel!
-    @IBOutlet weak var additionalInfoLabel: UILabel!
-    
-    
     
     var delegate: GuestCheckoutTableViewCellDelegate?
     
@@ -67,18 +55,6 @@ class GuestCheckoutTableViewCell: UITableViewCell, UITextFieldDelegate {
         }
         
         self.localizedLabels()
-        self.firstNameLabel.required()
-        self.lastNameLabel.required()
-        self.mobileNumberLabel.required()
-        self.emailAddressLabel.required()
-        
-        self.streetNameLabel.required()
-        
-        self.provinceLabel.required()
-        self.cityLabel.required()
-        self.barangayLabel.required()
-        
-        self.zipCodelabel.required()
         
         self.clipsToBounds = true
     }
@@ -88,16 +64,10 @@ class GuestCheckoutTableViewCell: UITableViewCell, UITextFieldDelegate {
         self.lastNameLabel.text = AddressStrings.lastName
         self.mobileNumberLabel.text = AddressStrings.mobileNumber
         self.emailAddressLabel.text = AddressStrings.emailAddress
-        self.unitNoLabel.text = AddressStrings.unitNo
-        self.buildingNameLabel.text = AddressStrings.buildingName
-        self.streetNumberLabel.text = AddressStrings.streetNo
-        self.streetNameLabel.text = AddressStrings.streetName
-        self.subdivisionLabel.text = AddressStrings.subdivision
         self.provinceLabel.text = AddressStrings.province
         self.cityLabel.text = AddressStrings.city
         self.barangayLabel.text = AddressStrings.barangay
         self.zipCodelabel.text = AddressStrings.zipCode
-        self.additionalInfoLabel.text = AddressStrings.additionalInfo
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
@@ -111,11 +81,12 @@ class GuestCheckoutTableViewCell: UITableViewCell, UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        if textField != self.additionalInfoTextField {
+        if textField != self.zipCodeTextField {
             self.delegate?.guestCheckoutTableViewCell(self, didClickNext: textField.tag, textField: textField)
         } else {
             self.delegate?.guestCheckoutTableViewCell(self, didClickDone: textField.tag, textField: textField)
         }
+        
         return true
     }
     
