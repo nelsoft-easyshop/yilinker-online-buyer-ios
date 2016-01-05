e0cb9021fc055c1c5a4cd2c63aa87b6c5cc34411
ajtandoc <alvin.tandoc@easyshop.ph>
2016-01-05 11:36:48 +0800

    Added request error type enum.

---
+++ YiLinkerOnlineBuyer/resources/Helper/WebServiceManager.swift
@@ -0,0 +1,13 @@
+//
+//  WebServiceManager.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 1/5/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class WebServiceManager: NSObject {
+   
+}
