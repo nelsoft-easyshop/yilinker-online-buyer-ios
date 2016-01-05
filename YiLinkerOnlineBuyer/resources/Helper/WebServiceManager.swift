b9e2b42887343c820a3f66d113792c409d531ecc
ajtandoc <alvin.tandoc@easyshop.ph>
2016-01-05 18:05:20 +0800

    Refactored web service manager status code.

--- YiLinkerOnlineBuyer/resources/Helper/WebServiceManager.swift
+++ YiLinkerOnlineBuyer/resources/Helper/WebServiceManager.swift
@@ -26,24 +26,84 @@ class WebServiceManager: NSObject {
         
         let parameters: NSDictionary = [self.emailKey: emailAddress, self.passwordKey: password, self.clientIdKey: Constants.Credentials.clientID(), self.clientSecretKey: Constants.Credentials.clientSecret(), self.grantTypeKey: Constants.Credentials.grantBuyer]
         
+        self.firePostRequestWithUrl(url, parameters: parameters) { (successful, responseObject, requestErrorType) -> Void in
+            actionHandler(successful: successful, responseObject: responseObject, requestErrorType: requestErrorType)
+        }
+    }
+    
+    class func fireGetHomePageDataWithUrl(url: String, actionHandler: (successful: Bool, responseObject: AnyObject, requestErrorType: RequestErrorType) -> Void) {
+        self.fireGetRequestWithUrl(url, parameters: []) { (successful, responseObject, requestErrorType) -> Void in
+            actionHandler(successful: successful, responseObject: responseObject, requestErrorType: requestErrorType)
+        }
+    }
+    
+    //MARK: - Post Request With Url
+    //This function is for removing repeated codes in handler
+    private static func firePostRequestWithUrl(url: String, parameters: AnyObject, actionHandler: (successful: Bool, responseObject: AnyObject, requestErrorType: RequestErrorType) -> Void) {
+        let manager = APIManager.sharedInstance
         if Reachability.isConnectedToNetwork() {
-            manager.POST(url, parameters: parameters, success: {
+            manager.GET(url, parameters: parameters, success: {
                 (task: NSURLSessionDataTask!, responseObject: AnyObject!) in
-                 actionHandler(successful: true, responseObject: responseObject, requestErrorType: .NoError)
+                actionHandler(successful: true, responseObject: responseObject, requestErrorType: .NoError)
                 }, failure: {
                     (task: NSURLSessionDataTask!, error: NSError!) in
                     if let task = task.response as? NSHTTPURLResponse {
                         if error.userInfo != nil {
+                            //Request is successful but encounter error in server
                             actionHandler(successful: false, responseObject: error.userInfo!, requestErrorType: .ResponseError)
                         } else if task.statusCode == Constants.WebServiceStatusCode.pageNotFound {
+                            //Page not found
                             actionHandler(successful: false, responseObject: [], requestErrorType: .PageNotFound)
                         } else if task.statusCode == Constants.WebServiceStatusCode.requestTimeOut {
+                            //Request Timeout
                             actionHandler(successful: false, responseObject: [], requestErrorType: .RequestTimeOut)
+                        } else if task.statusCode == Constants.WebServiceStatusCode.expiredAccessToken {
+                            //The accessToken is already expired
+                            actionHandler(successful: false, responseObject: [], requestErrorType: .AccessTokenExpired)
                         } else {
+                            //Unrecognized error, this is a rare case.
                             actionHandler(successful: false, responseObject: [], requestErrorType: .UnRecognizeError)
                         }
                     } else {
-                        actionHandler(successful: false, responseObject: [], requestErrorType: .NoInternetConnection)            
+                        //No internet connection
+                        actionHandler(successful: false, responseObject: [], requestErrorType: .NoInternetConnection)
+                    }
+            })
+        } else {
+            actionHandler(successful: false, responseObject: [], requestErrorType: .NoInternetConnection)
+        }
+    }
+    
+    //MARK: - Get Request With Url
+    //This function is for removing repeated codes in handler
+    private static func fireGetRequestWithUrl(url: String, parameters: AnyObject, actionHandler: (successful: Bool, responseObject: AnyObject, requestErrorType: RequestErrorType) -> Void) {
+        let manager = APIManager.sharedInstance
+        if Reachability.isConnectedToNetwork() {
+            manager.GET(url, parameters: parameters, success: {
+                (task: NSURLSessionDataTask!, responseObject: AnyObject!) in
+                actionHandler(successful: true, responseObject: responseObject, requestErrorType: .NoError)
+                }, failure: {
+                    (task: NSURLSessionDataTask!, error: NSError!) in
+                    if let task = task.response as? NSHTTPURLResponse {
+                        if error.userInfo != nil {
+                            //Request is successful but encounter error in server
+                            actionHandler(successful: false, responseObject: error.userInfo!, requestErrorType: .ResponseError)
+                        } else if task.statusCode == Constants.WebServiceStatusCode.pageNotFound {
+                            //Page not found
+                            actionHandler(successful: false, responseObject: [], requestErrorType: .PageNotFound)
+                        } else if task.statusCode == Constants.WebServiceStatusCode.requestTimeOut {
+                            //Request Timeout
+                            actionHandler(successful: false, responseObject: [], requestErrorType: .RequestTimeOut)
+                        } else if task.statusCode == Constants.WebServiceStatusCode.expiredAccessToken {
+                            //The accessToken is already expired
+                            actionHandler(successful: false, responseObject: [], requestErrorType: .AccessTokenExpired)
+                        } else {
+                            //Unrecognized error, this is a rare case.
+                            actionHandler(successful: false, responseObject: [], requestErrorType: .UnRecognizeError)
+                        }
+                    } else {
+                        //No internet connection
+                        actionHandler(successful: false, responseObject: [], requestErrorType: .NoInternetConnection)
                     }
             })
         } else {
