6667044b14ae8a8f1d14733ad51083d1fcb8fefb
ajtandoc <alvin.tandoc@easyshop.ph>
2016-01-06 10:49:45 +0800

    Added page not found in localized string.

--- YiLinkerOnlineBuyer/resources/Helper/Constants.swift
+++ YiLinkerOnlineBuyer/resources/Helper/Constants.swift
@@ -28,6 +28,7 @@ struct Constants {
         static let noInternet: String = StringHelper.localizedStringWithKey("NO_INTERNET_LOCALIZE_KEY")
         static let noInternetErrorMessage: String = StringHelper.localizedStringWithKey("NO_INTERNET_ERROR_MESSAGE_LOCALIZE_KEY")
         static let targetNotAvailable: String = StringHelper.localizedStringWithKey("TARGET_NOT_AVAILABLE")
+        static let pageNotFound: String = StringHelper.localizedStringWithKey("PAGE_NOT_FOUND_LOCALIZE_KEY")
     }
     
     struct HomePage {
