eb05407f5ac38f4e8b504abdc420bf916aa4bfb2
Rj <rj.constantino@easyshop.ph>
2016-01-07 18:18:03 +0800

    Resolved bottom text.

---
+++ YiLinkerOnlineBuyer/resources/View/ProductExtendedView.swift
@@ -0,0 +1,21 @@
+//
+//  ProductExtendedView.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Rj Constantino on 1/7/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class ProductExtendedView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
