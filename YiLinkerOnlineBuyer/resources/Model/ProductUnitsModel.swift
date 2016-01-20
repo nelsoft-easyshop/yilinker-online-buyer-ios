f51e98e4960bae2f5b8c2a7869ff662e8d7a0a8c
Rj <rj.constantino@easyshop.ph>
2016-01-20 15:07:11 +0800

    Enhanced parser in product details.

--- YiLinkerOnlineBuyer/resources/Model/ProductUnitsModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/ProductUnitsModel.swift
@@ -35,79 +35,115 @@ class ProductUnitsModel {
         var model = ProductUnitsModel()
         
         if dictionary.isKindOfClass(NSDictionary) {
-            
-            if let tempVar = dictionary["productUnitId"] as? String {
-                model.productUnitId = tempVar
-            }
-            
-            if let tempVar = dictionary["quantity"] as? Int {
-                model.quantity = tempVar
-            }
-            
-            if let tempVar = dictionary["sku"] as? String {
-                model.sku = tempVar
-            }
-            
-            if let tempVar = dictionary["price"] as? String {
-                model.price = tempVar
-            }
-            
-            if let tempVar = dictionary["discount"] as? Int {
-                model.discount = tempVar
-            }
-            
-            if let tempVar = dictionary["discountedPrice"] as? String {
-                model.discountedPrice = tempVar
-            }
-            
-            if let created: AnyObject = dictionary["dateCreated"] {
-                if let tempVar = created["date"] as? String {
-                    model.createdDate = tempVar
+            let keyExists = dictionary["productUnitId"] != nil
+            if keyExists {
+                if let tempVar = dictionary["productUnitId"] as? String {
+                    model.productUnitId = tempVar
+                } else {
+                    model.productUnitId = "0"
                 }
                 
-                if let tempVar = created["timezone_type"] as? Int {
-                    model.createdTimzeZoneType = tempVar
+                if let tempVar = dictionary["quantity"] as? Int {
+                    model.quantity = tempVar
+                } else {
+                    model.quantity = 0
                 }
                 
-                if let tempVar = created["timezone"] as? String {
-                    model.createdTimezone = tempVar
-                }
-            }
-            
-            if let lastModified: AnyObject = dictionary["dateLastModified"] {
-                if let tempVar = lastModified["date"] as? String {
-                    model.lastModifiedDate = tempVar
+                if let tempVar = dictionary["sku"] as? String {
+                    model.sku = tempVar
+                } else {
+                    model.sku = "0"
                 }
                 
-                if let tempVar = lastModified["timezone_type"] as? Int {
-                    model.lastModifiedTimeZoneType = tempVar
+                if let tempVar = dictionary["price"] as? String {
+                    model.price = tempVar
+                } else {
+                    model.price = "0"
                 }
                 
-                if let tempVar = lastModified["timezone"] as? String {
-                    model.lastModifiedTimeZone = tempVar
+                if let tempVar = dictionary["discount"] as? Int {
+                    model.discount = tempVar
+                } else {
+                    model.discount = 0
                 }
-
-            }
-            
-            if let tempVar = dictionary["status"] as? Int {
-                model.status = tempVar
-            }
-            
-            if let tempVar = dictionary["imageIds"] as? NSArray {
-                model.imageIds = tempVar as! [String]
-            }
-            
-            if let tempVar = dictionary["primaryImage"] as? String {
-                model.primaryImage = tempVar
-            }
-            
-            if let tempVar = dictionary["combination"] as? NSArray {
-                model.combination = tempVar as! [String]
-            }
-            
-            if let tempVar = dictionary["combinationNames"] as? NSArray {
-                model.combinationNames = tempVar as! [String]
-            }
+                
+                if let tempVar = dictionary["discountedPrice"] as? String {
+                    model.discountedPrice = tempVar
+                } else {
+                    model.discountedPrice = "0"
+                }
+                
+                if let created: AnyObject = dictionary["dateCreated"] {
+                    if let tempVar = created["date"] as? String {
+                        model.createdDate = tempVar
+                    } else {
+                        model.createdDate = "0"
+                    }
+                    
+                    if let tempVar = created["timezone_type"] as? Int {
+                        model.createdTimzeZoneType = tempVar
+                    } else {
+                        model.createdTimzeZoneType = 0
+                    }
+                    
+                    if let tempVar = created["timezone"] as? String {
+                        model.createdTimezone = tempVar
+                    } else {
+                        model.createdTimezone = "0"
+                    }
+                }
+                
+                if let lastModified: AnyObject = dictionary["dateLastModified"] {
+                    if let tempVar = lastModified["date"] as? String {
+                        model.lastModifiedDate = tempVar
+                    } else {
+                        model.lastModifiedDate = "0"
+                    }
+                    
+                    if let tempVar = lastModified["timezone_type"] as? Int {
+                        model.lastModifiedTimeZoneType = tempVar
+                    } else {
+                        model.lastModifiedTimeZoneType = 0
+                    }
+                    
+                    if let tempVar = lastModified["timezone"] as? String {
+                        model.lastModifiedTimeZone = tempVar
+                    } else {
+                        model.lastModifiedTimeZone = "0"
+                    }
+                    
+                }
+                
+                if let tempVar = dictionary["status"] as? Int {
+                    model.status = tempVar
+                } else {
+                    model.productUnitId = "0"
+                }
+                
+                if let tempVar = dictionary["imageIds"] as? NSArray {
+                    model.imageIds = tempVar as! [String]
+                } else {
+                    model.imageIds = []
+                }
+                
+                if let tempVar = dictionary["primaryImage"] as? String {
+                    model.primaryImage = tempVar
+                } else {
+                    model.primaryImage = ""
+                }
+                
+                if let tempVar = dictionary["combination"] as? NSArray {
+                    model.combination = tempVar as! [String]
+                } else {
+                    model.combination = []
+                }
+                
+                if let tempVar = dictionary["combinationNames"] as? NSArray {
+                    model.combinationNames = tempVar as! [String]
+                } else {
+                    model.combinationNames = []
+                }
+            } // if exists
             
         } // if dictionary
         
