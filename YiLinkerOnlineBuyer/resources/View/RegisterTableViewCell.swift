851ccbc0d7cc3c8ce8f4be906a84e4599e66d0dd
ajtandoc <alvin.tandoc@easyshop.ph>
2016-01-27 11:40:33 +0800

    Added register cell.

--- YiLinkerOnlineBuyer/resources/View/RegisterTableViewCell.swift
+++ YiLinkerOnlineBuyer/resources/View/RegisterTableViewCell.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class RegisterTableViewCell: UITableViewCell {
+protocol RegisterTableViewCellDelegate {
+    func registerTableViewCell(registerTableViewCell: RegisterTableViewCell, textFieldShouldReturn textField: UITextField)
+    func registerTableViewCell(registerTableViewCell: RegisterTableViewCell, didTapRegister button: UIButton)
+}
 
+class RegisterTableViewCell: UITableViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var reTypePasswordTextField: UITextField!
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    var delegate: RegisterTableViewCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.initTextViews()
+        self.registerButton.setTitle(RegisterStrings.registerMeNow, forState: UIControlState.Normal)
+        self.registerButton.layer.cornerRadius = 5
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -21,4 +41,34 @@ class RegisterTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    //MARK: - 
+    //MARK: - Register
+    @IBAction func register(sender: UIButton) {
+        self.delegate?.registerTableViewCell(self, didTapRegister: sender)
+    }
+    
+    //MARK: -
+    //MARK: - TextField Delegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.delegate?.registerTableViewCell(self, textFieldShouldReturn: textField)
+        return true
+    }
+    
+    //MARK: - 
+    //MARK: - Init Text Views
+    func initTextViews() {
+        self.firstNameTextField.attributedPlaceholder = StringHelper.required(RegisterStrings.firstName)
+        self.lastNameTextField.attributedPlaceholder = StringHelper.required(RegisterStrings.lastName)
+        self.emailAddressTextField.attributedPlaceholder = StringHelper.required(RegisterStrings.emailAddress)
+        self.passwordTextField.attributedPlaceholder = StringHelper.required(RegisterStrings.password)
+        self.reTypePasswordTextField.attributedPlaceholder = StringHelper.required(RegisterStrings.reTypePassword)
+        self.mobileNumberTextField.attributedPlaceholder = StringHelper.required(RegisterStrings.mobileNumber)
+        
+        self.firstNameTextField.delegate = self
+        self.lastNameTextField.delegate = self
+        self.emailAddressTextField.delegate = self
+        self.passwordTextField.delegate = self
+        self.reTypePasswordTextField.delegate = self
+        self.mobileNumberTextField.delegate = self
+    }
 }
