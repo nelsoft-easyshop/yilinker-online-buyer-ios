b2c7c5927b0fe6809791b826cc193cb180a3b4fa
ajtandoc <alvin.tandoc@easyshop.ph>
2016-01-28 16:56:38 +0800

    Added header for feedback tableview.

--- YiLinkerOnlineBuyer/resources/View/FeedBackTableViewCell.swift
+++ YiLinkerOnlineBuyer/resources/View/FeedBackTableViewCell.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
+protocol FeedBackTableViewCellDelegate {
+    func feedBackTableViewCell(feedBackTableViewCell: FeedBackTableViewCell, didTapCancel cancelButton: UIButton)
+}
+
 class FeedBackTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var starFrameView: UIView!
+    @IBOutlet weak var rateFrameView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var peopleLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var numberOfPeopleLabel: UILabel!       
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    var delegate: FeedBackTableViewCellDelegate?
+    
+    @IBOutlet weak var starOneButton: UIButton!
+    @IBOutlet weak var starTwoButton: UIButton!
+    @IBOutlet weak var starThreeButton: UIButton!
+    @IBOutlet weak var starFourButton: UIButton!
+    @IBOutlet weak var starFiveButton: UIButton!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.rateFrameView.layer.cornerRadius = self.rateFrameView.frame.size.width / 2
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -21,4 +42,22 @@ class FeedBackTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    //MARK: - 
+    //MARK: - Set Seller Rating
+    func setSellerRating(rating: Int) {
+        for button in self.starFrameView.subviews {
+            if button.isKindOfClass(UIButton) && button.tag != 0 {
+                if button.tag <= rating {
+                    button.setImage(UIImage(named: "rating2"), forState: UIControlState.Normal)
+                    //self.starOneButton.setImage(UIImage(named: "rating2"), forState: UIControlState.Normal)
+                }
+            }
+        }
+    }
+    
+    //MARK: - 
+    //MARK: - Cancel
+    @IBAction func cancel(sender: UIButton) {
+        self.delegate?.feedBackTableViewCell(self, didTapCancel: sender)
+    }
 }
