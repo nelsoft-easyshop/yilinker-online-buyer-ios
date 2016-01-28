e771eb8082e01498f9dbbbf4c23ca86379989f98
ajtandoc <alvin.tandoc@easyshop.ph>
2016-01-28 16:54:12 +0800

    Refactor cell identifier.

---
+++ YiLinkerOnlineBuyer/resources/View/FeedBackTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedBackTableViewCell.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 1/27/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class FeedBackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
