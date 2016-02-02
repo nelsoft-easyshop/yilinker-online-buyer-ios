8b0aa4772bcfd37e7cbab5c37c229220a304e15d
ajtandoc <alvin.tandoc@easyshop.ph>
2016-02-02 13:49:08 +0800

    Added delegate func for detecting the editing begin of cell.

---
+++ YiLinkerOnlineBuyer/resources/View/IncompleteRequirementsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IncompleteRequirementsTableViewCell.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by Alvin John Tandoc on 2/1/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class IncompleteRequirementsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
