335694adeb05018e9167852a36e6b64706b61e00
ajtandoc <alvin.tandoc@easyshop.ph>
2016-02-03 15:12:22 +0800

    Added success modal.

--- YiLinkerOnlineBuyer/resources/ViewController/SuccessModalViewController.swift
+++ YiLinkerOnlineBuyer/resources/ViewController/SuccessModalViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
+protocol SuccessModalViewControllerDelegate {
+    func successModalViewController(successModalViewController: SuccessModalViewController, didTapButton doneButton: UIButton)
+}
+
 class SuccessModalViewController: UIViewController {
 
+    @IBOutlet weak var circleContainerView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var checkImageView: UIImageView!
+    
+    var delegate: SuccessModalViewControllerDelegate?
+    
+    //MARK: -
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.circleContainerView.layer.cornerRadius = self.circleContainerView.frame.size.width / 2
+        self.doneButton.layer.cornerRadius = 5
+        self.containerView.layer.cornerRadius = 5
+        self.checkImageView.layer.zPosition = 100
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +37,9 @@ class SuccessModalViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - 
+    //MARK: - Done
+    @IBAction func done(sender: UIButton) {
+        self.delegate?.successModalViewController(self, didTapButton: sender)
     }
-    */
-
 }
