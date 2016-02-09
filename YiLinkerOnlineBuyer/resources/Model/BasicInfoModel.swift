ebe47c250da7d5fbc6af3cdcb0d32049b87e7b84
ajtandoc <alvin.tandoc@easyshop.ph>
2016-02-09 17:48:32 +0800

    Added basic info model.

--- YiLinkerOnlineBuyer/resources/Model/BasicInfoModel.swift
+++ YiLinkerOnlineBuyer/resources/Model/BasicInfoModel.swift
@@ -9,5 +9,37 @@
 import UIKit
 
 class BasicInfoModel: NSObject {
-   
+    var isVerified: Bool = false
+    var isSuccessful: Bool = false
+    var message: String = ""
+    
+    override init() {}
+    
+    init(isVerified: Bool, isSuccessful: Bool, message: String) {
+        self.isVerified = isVerified
+        self.isSuccessful = isSuccessful
+        self.message = message
+    }
+    
+    class func parseDataFromDictionary(dictionary: NSDictionary) -> BasicInfoModel {
+        var isVerified: Bool = false
+        var isSuccessful: Bool = false
+        var message: String = ""
+        
+        if let temp = dictionary["isSuccessful"] as? Bool {
+            isSuccessful = temp
+        }
+        
+        if let dataDictionary = dictionary["data"] as? NSDictionary {
+            if let temp = dataDictionary["isVerified"] as? Bool {
+                isVerified = temp
+            }
+        }
+        
+        if let temp = dictionary["message"] as? String {
+            message = temp
+        }
+        
+        return BasicInfoModel(isVerified: isVerified, isSuccessful: isSuccessful, message: message)
+    }
 }
