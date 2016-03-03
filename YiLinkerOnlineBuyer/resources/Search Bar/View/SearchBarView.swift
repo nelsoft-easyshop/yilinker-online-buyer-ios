6365e47cd329393332fd8a12c06601c43fd014e8
jpChan15 <john.chan@yilinker.ph>
2016-03-03 13:58:37 +0800

    Added search bar files.

---
+++ YiLinkerOnlineBuyer/resources/Search Bar/View/SearchBarView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchBarView.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by John Paul Chan on 3/3/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
