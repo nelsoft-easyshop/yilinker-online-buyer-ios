2089b172bea4d8c9c7538aa7aa9a1cf7a5a38ef4
jpChan15 <john.chan@yilinker.ph>
2016-03-04 12:28:38 +0800

    Added cell.

---
+++ YiLinkerOnlineBuyer/resources/Search Bar/View/SearchTypeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTypeTableViewCell.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by John Paul Chan on 3/3/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class SearchTypeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
