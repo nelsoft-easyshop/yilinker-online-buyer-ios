9916d6df49abbf169fdd8c5ca1ff92cb1fc36e46
jpChan15 <john.chan@yilinker.ph>
2016-03-22 12:17:56 +0800

    Implemented suggestions feedback.

--- YiLinkerOnlineBuyer/resources/Suggestions/View Controller/SuggestionViewController.swift
+++ YiLinkerOnlineBuyer/resources/Suggestions/View Controller/SuggestionViewController.swift
@@ -8,12 +8,57 @@
 
 import UIKit
 
+struct SuggestionStrings {
+    static let header = StringHelper.localizedStringWithKey("SUGGESTION_HEADLINE")
+    static let subHeader = StringHelper.localizedStringWithKey("SUGGESTION_SUB_HEADLINE")
+    static let title = StringHelper.localizedStringWithKey("SUGGESTION_TITLE")
+    static let titlePlaceholder = StringHelper.localizedStringWithKey("SUGGESTION_TITLE_LABEL")
+    static let descriptionPlaceholder = StringHelper.localizedStringWithKey("SUGGESTION_DESCRIPTION")
+    static let send = StringHelper.localizedStringWithKey("SUGGESTION_SEND")
+    static let titleRequiredError = StringHelper.localizedStringWithKey("SUGGESTION_TITLE_REQUIRED")
+    static let descriptionRequiredError = StringHelper.localizedStringWithKey("SUGGESTION_DESC_REQUIRED")
+}
+
+struct SuggestionNotifications {
+    static let validationErrorNotification = "validationErrorNotification"
+    static let saveFeedbackNotification = "saveFeedbackNotification"
+    static let refreshTokenErrorNotification = "refreshTokenErrorNotification"
+    static let statusChangedNotification = "statusChangedNotification"
+}
+
 class SuggestionViewController: UIViewController {
 
+    private let suggestionViewModel = SuggestionViewModel()
+    
+    @IBOutlet weak var headLineLabel: UILabel!
+    @IBOutlet weak var subLineLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var topMarginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var notificationCenter = NSNotificationCenter.defaultCenter()
+    
+    var originalTopMarginConstant: CGFloat =  0
+    var toYconstraint: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.initializeViews()
+        self.addBackButton()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.registerNotificationObserver()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterNotificationObserver()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +66,214 @@ class SuggestionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func registerNotificationObserver() { 
+        //Register notification observer
+        notificationCenter.addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: Selector("onValidationErrorNotification:"), name: SuggestionNotifications.validationErrorNotification, object: nil)
+        notificationCenter.addObserver(self, selector: Selector("onSaveFeedbackNotification:"), name: SuggestionNotifications.saveFeedbackNotification, object: nil)
+        notificationCenter.addObserver(self, selector: Selector("onRefreshTokenErrorNotification:"), name: SuggestionNotifications.refreshTokenErrorNotification, object: nil)
+        notificationCenter.addObserver(self, selector: Selector("onStatusChangedNotification:"), name: SuggestionNotifications.statusChangedNotification, object: nil)
     }
-    */
+    
+    func unregisterNotificationObserver() {
+        //unegister notification observer
+        notificationCenter.removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.removeObserver(self, name: SuggestionNotifications.validationErrorNotification, object: nil)
+        notificationCenter.removeObserver(self, name: SuggestionNotifications.saveFeedbackNotification, object: nil)
+        notificationCenter.removeObserver(self, name: SuggestionNotifications.refreshTokenErrorNotification, object: nil)
+    }
+    
+    func initializeViews() {
+        //Avoid overlapping of tab bar and navigation bar to the mainview
+        if self.respondsToSelector("edgesForExtendedLayout") {
+            self.edgesForExtendedLayout = UIRectEdge.None
+        }
+        
+        self.title = SuggestionStrings.title
+        self.sendButton.layer.cornerRadius = 5
+        self.descriptionTextView.layer.cornerRadius = 5
+        self.descriptionTextView.layer.borderWidth = 1
+        self.descriptionTextView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.titleTextField.layer.cornerRadius = 5
+        self.titleTextField.layer.borderWidth = 1
+        self.titleTextField.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        self.headLineLabel.text = SuggestionStrings.header
+        self.subLineLabel.text = SuggestionStrings.subHeader
+        self.descriptionTextView.text = SuggestionStrings.descriptionPlaceholder
+        self.titleTextField.text = SuggestionStrings.titlePlaceholder
+        self.sendButton.setTitle(SuggestionStrings.send, forState: .Normal)
+        
+        self.titleTextField.delegate = self
+        self.descriptionTextView.delegate = self
+        
+        if IphoneType.isIphone4() {
+            self.topMarginConstraint.constant = 12
+            self.toYconstraint = -120
+        } else if IphoneType.isIphone5() {
+            self.toYconstraint = -80
+        } else if IphoneType.isIphone6() {
+            self.toYconstraint = 20
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: "closeKeyboard")
+        self.view.addGestureRecognizer(tap)
+        
+        self.originalTopMarginConstant = self.topMarginConstraint.constant
+    }
+    
+    //Add cutomize back button to the navigation bar
+    func addBackButton() {
+        var backButton:UIButton = UIButton.buttonWithType(UIButtonType.Custom) as! UIButton
+        backButton.frame = CGRectMake(0, 0, 40, 40)
+        backButton.addTarget(self, action: "back", forControlEvents: UIControlEvents.TouchUpInside)
+        backButton.setImage(UIImage(named: "back-white"), forState: UIControlState.Normal)
+        var customBackButton:UIBarButtonItem = UIBarButtonItem(customView: backButton)
+        
+        let navigationSpacer: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FixedSpace, target: nil, action: nil)
+        navigationSpacer.width = -20
+        self.navigationItem.leftBarButtonItems = [navigationSpacer, customBackButton]
+    }
+    
+    func back() {
+        self.navigationController!.popViewControllerAnimated(true)
+    }
+    
+    func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func sendAction(sender: UIButton) {
+        self.closeKeyboard()
+        
+        if self.titleTextField.text == SuggestionStrings.titlePlaceholder {
+            self.suggestionViewModel.title = ""
+        } else {
+            self.suggestionViewModel.title = self.titleTextField.text
+        }
+        
+        if self.descriptionTextView.text == SuggestionStrings.descriptionPlaceholder {
+            self.suggestionViewModel.description = ""
+        } else {
+            self.suggestionViewModel.description = self.descriptionTextView.text
+        }
+        
+        self.suggestionViewModel.handleSavePressed()
+    }
+    
+    func setStatusOngoing(isOngoing: Bool) {
+        
+        self.sendButton.enabled = !isOngoing
+        self.titleTextField.enabled = !isOngoing
+        self.descriptionTextView.editable = !isOngoing
+        
+        if isOngoing {
+            self.sendButton.setTitle("", forState: .Normal)
+            self.sendButton.alpha = 0.5
+            self.activityIndicator.startAnimating()
+        } else {
+            self.sendButton.setTitle(SuggestionStrings.send, forState: .Normal)
+            self.sendButton.alpha = 1
+            self.activityIndicator.stopAnimating()
+            self.titleTextField.text = SuggestionStrings.titlePlaceholder
+            self.descriptionTextView.text = SuggestionStrings.descriptionPlaceholder
+        }
+    }
+    
+    // MARK: Notification Handler
+    
+    // Keyboard notification function
+    /* Update frame of whole view when keyboard is showing by subtracting keyboard height to
+    * original Y origin of view and adding the tab bar height */
+    func keyboardWillShow (notification: NSNotification){
+        if let userInfo = notification.userInfo {
+            if let keyboardHeight = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size.height {
+                self.topMarginConstraint.constant = self.toYconstraint
+                UIView.animateWithDuration(0.35, animations: { () -> Void in
+                    self.view.layoutIfNeeded()
+                    }, completion: nil)
+            }
+        }
+    }
+    
+    /* Update frame of whole view when keyboard is hiding by setting its original frame*/
+    func keyboardWillHide(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardHeight = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size.height {
+                self.topMarginConstraint.constant = self.originalTopMarginConstant
+                UIView.animateWithDuration(0.35, animations: { () -> Void in
+                    self.view.layoutIfNeeded()
+                    }, completion: nil)
+            }
+        }
+    }
+    
+    func onValidationErrorNotification(notification: NSNotification) {
+        if let errorMessage = notification.object as? String {
+            Toast.displayToastWithMessage(errorMessage, duration: 1.5, view: self.navigationController!.view)
+        }
+    }
+    
+    func onSaveFeedbackNotification(notification: NSNotification) {
+        if let responseObject = notification.object as? NSDictionary {
+            if let message = responseObject["message"] as? String {
+                Toast.displayToastWithMessage(message, duration: 1.5, view: self.navigationController!.view)
+            }
+        }
+    }
+    
+    func onRefreshTokenErrorNotification(notification: NSNotification) {
+        //Show UIAlert and force the user to logout
+        UIAlertController.displayAlertRedirectionToLogin(self, actionHandler: { (sucess) -> Void in
+            SessionManager.logoutWithTarget(self)
+        })
+    }
+    
+    func onStatusChangedNotification(notification: NSNotification) {
+        if let status = notification.object as? Bool {
+            self.setStatusOngoing(status)
+        }
+    }
+}
 
+extension SuggestionViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textView.text == SuggestionStrings.descriptionPlaceholder {
+            textView.text = ""
+            textView.textColor = UIColor.darkGrayColor()
+        }
+        
+        textView.becomeFirstResponder()
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text == "" {
+            textView.text = SuggestionStrings.descriptionPlaceholder
+            textView.textColor = UIColor.lightGrayColor()
+        }
+        
+        textView.resignFirstResponder()
+    }
+}
+
+extension SuggestionViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.text == SuggestionStrings.titlePlaceholder {
+            textField.text = ""
+            textField.textColor = UIColor.darkGrayColor()
+        }
+        
+        textField.becomeFirstResponder()
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if textField.text == "" {
+            textField.text = SuggestionStrings.titlePlaceholder
+            textField.textColor = UIColor.darkGrayColor()
+        }
+        
+        textField.resignFirstResponder()
+    }
 }
--- YiLinkerOnlineBuyer/resources/Suggestions/View Model/SuggestionViewModel.swift
+++ YiLinkerOnlineBuyer/resources/Suggestions/View Model/SuggestionViewModel.swift
@@ -6,8 +6,110 @@
 //  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class SuggestionViewModel: NSObject {
+public class SuggestionViewModel {
+    
+    public var title: String = ""
+    public var description: String = ""
+    public var phoneModel: String = ""
+    public var osVersion: String = ""
+    public var osName: String = ""
+    
+    let notificationCenter = NSNotificationCenter.defaultCenter()
+    var errorNotification: NSNotification?
+    var statusNotification: NSNotification?
+    var saveFeedbackNotification: NSNotification?
    
+    public init() {
+        self.phoneModel = UIDevice().deviceModel
+        self.osVersion = UIDevice.currentDevice().systemVersion
+        self.osName = "iOS"
+        
+    }
+    
+    //MARK: - Validations
+    func validateTitle() -> Bool {
+        return self.title.isNotEmpty() ? true : false
+    }
+    
+    func validateDescription() -> Bool {
+        return self.description.isNotEmpty() ? true : false
+    }
+    
+    //MARK: - Save Button Handler
+    func handleSavePressed() {
+        if !self.validateTitle() {
+            self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: SuggestionStrings.titleRequiredError)
+            notificationCenter.postNotification(self.errorNotification!)
+        } else if !self.validateDescription() {
+            self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: SuggestionStrings.descriptionRequiredError)
+            notificationCenter.postNotification(self.errorNotification!)
+        } else {
+            self.fireSaveFeedback()
+        }
+    }
+    
+    //MARK: -
+    //MARK: - API Requests
+    
+    //MARK: - Save Feedback
+    func fireSaveFeedback() {
+        statusNotification = NSNotification(name: SuggestionNotifications.statusChangedNotification, object: true)
+        notificationCenter.postNotification(statusNotification!)
+        
+        WebServiceManager.fireSaveFeedBackWithUrl(APIAtlas.mobileFeedBack(), title: self.title, description: self.description, phoneModel: self.phoneModel, osVersion: self.osVersion, osName: self.osName, access_token: SessionManager
+            .accessToken()) { (successful, responseObject, requestErrorType) -> Void in
+            
+            self.statusNotification = NSNotification(name: SuggestionNotifications.statusChangedNotification, object: false)
+            self.notificationCenter.postNotification(self.statusNotification!)
+            println(responseObject)
+            if successful {
+                self.saveFeedbackNotification = NSNotification(name: SuggestionNotifications.saveFeedbackNotification, object: responseObject)
+                println(responseObject)
+                self.notificationCenter.postNotification(self.saveFeedbackNotification!)
+            } else {
+                
+                if requestErrorType == .ResponseError {
+                    //Error in api requirements
+                    let errorModel: ErrorModel = ErrorModel.parseErrorWithResponce(responseObject as! NSDictionary)
+                    self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: errorModel.message)
+                    self.notificationCenter.postNotification(self.errorNotification!)
+                } else if requestErrorType == .AccessTokenExpired {
+                    self.fireRefreshToken()
+                } else {
+                    if requestErrorType == .PageNotFound {
+                        //Page not found
+                        self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: Constants.Localized.pageNotFound)
+                    } else if requestErrorType == .NoInternetConnection {
+                        //No internet connection
+                        self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: Constants.Localized.noInternetErrorMessage)
+                    } else if requestErrorType == .RequestTimeOut {
+                        //Request timeout
+                        self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: Constants.Localized.noInternetErrorMessage)
+                    } else if requestErrorType == .UnRecognizeError {
+                        //Unhandled error
+                        self.errorNotification = NSNotification(name: SuggestionNotifications.validationErrorNotification, object: Constants.Localized.error)
+                    }
+                    
+                    self.notificationCenter.postNotification(self.errorNotification!)
+                }
+            }
+        }
+    }
+    
+    //MARK: - Fire Refresh Token
+    func fireRefreshToken() {
+        WebServiceManager.fireRefreshTokenWithUrl(APIAtlas.refreshTokenUrl, actionHandler: {
+            (successful, responseObject, requestErrorType) -> Void in
+            if successful {
+                SessionManager.parseTokensFromResponseObject(responseObject as! NSDictionary)
+                self.fireSaveFeedback()
+            } else {
+                let accessTokenErrorNotification = NSNotification(name: SuggestionNotifications.refreshTokenErrorNotification, object: nil)
+                self.notificationCenter.postNotification(accessTokenErrorNotification)
+            }
+        })
+    }
+    
 }
