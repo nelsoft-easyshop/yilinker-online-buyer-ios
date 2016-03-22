3423448ba945eb84de365837496d86634777bdfe
jpChan15 <john.chan@yilinker.ph>
2016-03-22 12:16:07 +0800

    Added suggesti8on feddback viewcontroller.

---
+++ YiLinkerOnlineBuyer/resources/Suggestions/View Controller/SuggestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SuggestionViewController.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by John Paul Chan on 3/16/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YiLinkerOnlineBuyer/resources/Suggestions/View Model/SuggestionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestionViewModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by John Paul Chan on 3/17/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionViewModel: NSObject {
+   
+}
