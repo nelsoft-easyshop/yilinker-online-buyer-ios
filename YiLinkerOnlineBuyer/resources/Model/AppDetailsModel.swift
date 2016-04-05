c76811cf7cd37aa1b827bb7928e9e5ee77b73bd0
jpChan15 <john.chan@yilinker.ph>
2016-04-05 17:30:07 +0800

    Implemented check for updates and what's new

---
+++ YiLinkerOnlineBuyer/resources/Model/AppDetailsModel.swift
@@ -0,0 +1,13 @@
+//
+//  AppDetailsModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by John Paul Chan on 4/5/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class AppDetailsModel: NSObject {
+   
+}
