1c17f917f1e2cb168e5da69bc0aa0ae3f0989d8e
jpChan15 <john.chan@yilinker.ph>
2016-04-20 09:57:44 +0800

    Added language model.

---
+++ YiLinkerOnlineBuyer/resources/Profile/Model/LanguageModel.swift
@@ -0,0 +1,13 @@
+//
+//  LanguageModel.swift
+//  YiLinkerOnlineBuyer
+//
+//  Created by John Paul Chan on 4/19/16.
+//  Copyright (c) 2016 yiLinker-online-buyer. All rights reserved.
+//
+
+import UIKit
+
+class LanguageModel: NSObject {
+   
+}
