507ba92e4e668d0066e73adc861b84015a78789c
Rj <rj.constantino@yilinker.ph>
2016-07-12 10:33:33 +0800

    Added format for model.

--- YiLinkerOnlineBuyer/resources/Home/Model/LayoutTwelveModel.swift
+++ YiLinkerOnlineBuyer/resources/Home/Model/LayoutTwelveModel.swift
@@ -9,5 +9,54 @@
 import UIKit
 
 class LayoutTwelveModel: NSObject {
-   
+    var layoutId: Int = 0
+    var sectionTitle: String = ""
+    var isViewMoreAvailable: Bool = false
+    var viewMoreTarget: TargetModel = TargetModel()
+    var data: [HomeProductModel] = []
+    
+    override init() {
+        
+    }
+    
+    init(layoutId: Int, sectionTitle: String, isViewMoreAvailable: Bool, viewMoreTarget: TargetModel, data: [HomeProductModel]) {
+        self.layoutId = layoutId
+        self.sectionTitle = sectionTitle
+        self.isViewMoreAvailable = isViewMoreAvailable
+        self.viewMoreTarget = viewMoreTarget
+        self.data = data
+    }
+    
+    class func parseDataFromDictionary(dictionary: NSDictionary) -> LayoutTwelveModel {
+        var layoutId: Int = 0
+        var sectionTitle: String = ""
+        var isViewMoreAvailable: Bool = false
+        var viewMoreTarget: TargetModel = TargetModel()
+        var remainingTime: String = ""
+        var data: [HomeProductModel] = []
+        
+        if let temp = dictionary["layoutId"] as? Int {
+            layoutId = temp
+        }
+        
+        if let temp = dictionary["sectionTitle"] as? String {
+            sectionTitle = temp
+        }
+        
+        if let temp = dictionary["isViewMoreAvailable"] as? Bool {
+            isViewMoreAvailable = temp
+        }
+        
+        if let temp = dictionary["viewMoreTarget"] as? NSDictionary {
+            viewMoreTarget = TargetModel.parseDataFromDictionary(temp)
+        }
+        
+        if let temps = dictionary["data"] as? [NSDictionary] {
+            for temp in temps {
+                data.append(HomeProductModel.parseDataFromDictionary(temp))
+            }
+        }
+        
+        return LayoutTwelveModel(layoutId: layoutId, sectionTitle: sectionTitle, isViewMoreAvailable: isViewMoreAvailable, viewMoreTarget: viewMoreTarget, data: data)
+    }
 }
